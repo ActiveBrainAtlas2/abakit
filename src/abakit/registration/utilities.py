124ecb01de8d60875e854ab2aa48971d89d542c7
madwilliam5225 <madwilliam5225@gmail.com>
2021-11-08 10:32:59 -0800

    updates`

--- src/abakit/registration/utilities.py
+++ src/abakit/registration/utilities.py
@@ -1,35 +1,45 @@
 from .algorithm import umeyama
 import numpy as np
 import copy
-def get_rigid_transformation_from_dicts(com_dict1,com_dict2):
+
+def make_common_dictionary_entry_into_array(com_dict1,com_dict2):
     common_landmarks = (set(com_dict1.keys()).intersection(set(com_dict2.keys())))
     com_dict1 = np.array([com_dict1[landmark] for landmark in common_landmarks])
     com_dict2 = np.array([com_dict2[landmark] for landmark in common_landmarks])
-    rigid_transformation = umeyama(com_dict1.T,com_dict2.T)
-    return rigid_transformation
+    return com_dict1,com_dict2,common_landmarks
 
-def get_and_apply_transform(moving_com,still_com):
-    affine_transform = umeyama(moving_com.T,still_com.T)
-    transformed_coms = apply_rigid_transform_to_points(moving_com,affine_transform)
-    return transformed_coms,affine_transform
+def get_similarity_transformation_from_dicts(moving,fixed):
+    moving,fixed,_ = make_common_dictionary_entry_into_array(moving,fixed)
+    similarity_transformation = umeyama(moving.T,fixed.T)
+    return similarity_transformation
 
-def apply_rigid_transformation_to_com(com,rigid_transformation):
-    rotation,translation = rigid_transformation
+def get_and_apply_similarity_transform_to_dictionaries(moving,fixed):
+    moving,fixed,common_landmarks = make_common_dictionary_entry_into_array(moving,fixed)
+    transformed_coms,_= get_and_apply_similarity_transform(moving,fixed)
+    return dict(zip(common_landmarks,transformed_coms))
+
+def get_and_apply_similarity_transform(moving_com,fixed_com):
+    similarity_transform = umeyama(moving_com.T,fixed_com.T)
+    transformed_coms = apply_similarity_transform_to_points(moving_com,similarity_transform)
+    return transformed_coms,similarity_transform
+
+def apply_similarity_transformation_to_com(com,similarity_transformation):
+    rotation,translation = similarity_transformation
     return rotation@np.array(com).reshape(3)+ translation.reshape(3)
 
-def apply_rigid_transform_to_points(coms,rigid_transform):
+def apply_similarity_transform_to_points(coms,similarity_transform):
     transformed_com_list = []
     for com in coms:
-        transformed_com = apply_rigid_transformation_to_com(com,rigid_transform)
+        transformed_com = apply_similarity_transformation_to_com(com,similarity_transform)
         transformed_com_list.append(transformed_com)
     return np.array(transformed_com_list)
 
-def apply_rigid_transformation_to_com_dict(com_dict,rigid_transformation):
+def apply_similarity_transformation_to_com_dict(com_dict,similarity_transformation):
     for landmark,com in com_dict.items():
-        com_dict[landmark] = apply_rigid_transformation_to_com(com,rigid_transformation)
+        com_dict[landmark] = apply_similarity_transformation_to_com(com,similarity_transformation)
     return com_dict
 
-def apply_rigid_transformation_to_com_dict_list(com_dict_list,rigid_transformation):
+def apply_similarity_transformation_to_com_dict_list(com_dict_list,similarity_transformation):
     com_dict_list_copy = copy.deepcopy(com_dict_list)
-    rigid_transformed_com_dicts = [apply_rigid_transformation_to_com_dict(com_dict,rigid_transformation) for com_dict in com_dict_list_copy]
-    return rigid_transformed_com_dicts
+    similarity_transformed_com_dicts = [apply_similarity_transformation_to_com_dict(com_dict,similarity_transformation) for com_dict in com_dict_list_copy]
+    return similarity_transformed_com_dicts
