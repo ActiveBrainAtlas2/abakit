0b58febd35f844b5f90d65dd8640dc305c5ae1fc
madwilliam <madwilliam5225@gmail.com>
2022-04-13 13:35:30 -0700

    updates

--- setup.py
+++ setup.py
@@ -2,5 +2,11 @@
 #   https://setuptools.readthedocs.io/en/latest/userguide/quickstart.html#development-mode
 
 import setuptools
+from distutils.util import convert_path
 
-setuptools.setup()
+main_ns = {}
+ver_path = convert_path('mymodule/version.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
+
+setuptools.setup(version=main_ns['__version__'])
