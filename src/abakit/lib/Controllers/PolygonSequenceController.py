9b9ecc103e3fed18c56adecf91efdea8170dc9db
madwilliam <madwilliam5225@gmail.com>
2022-05-04 08:54:55 -0700

    updates

--- src/abakit/lib/Controllers/PolygonSequenceController.py
+++ src/abakit/lib/Controllers/PolygonSequenceController.py
@@ -8,3 +8,4 @@ from abakit.lib.Controllers.Controller import Controller
 class PolygonSequenceController(Controller):
     def get_available_polygons(self):
         active_sessions = self.session.query(PolygonSequence).distinct(PolygonSequence.FK_session_id).all()
+        information = [[i.session.FK_prep_id,i.FK_annotator_id,i.brain_region.abbreviation] for i in active_sessions]
