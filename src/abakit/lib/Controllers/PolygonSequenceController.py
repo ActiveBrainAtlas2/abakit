5bf834ecc6b686dae12cba4928340a710a494a59
madwilliam <madwilliam5225@gmail.com>
2022-05-04 09:17:24 -0700

    updates

--- src/abakit/lib/Controllers/PolygonSequenceController.py
+++ src/abakit/lib/Controllers/PolygonSequenceController.py
@@ -1,6 +1,6 @@
 import numpy as np
 from abakit.model.annotation_points import PolygonSequence
-from abakit.model.annotation_session import AnnotationSession
+from abakit.model.annotation_session import AnnotationSession,AnnotationType
 from abakit.lib.annotation_layer import Volume
 import json
 import pandas as pd
@@ -8,8 +8,10 @@ from abakit.lib.Controllers.Controller import Controller
 
 class PolygonSequenceController(Controller):
     def get_available_volumes(self):
-        active_sessions = self.session.query(PolygonSequence).distinct(PolygonSequence.FK_session_id).all()
-        information = [[i.session.FK_prep_id,i.user,i.brain_region.abbreviation] for i in active_sessions]
+        active_sessions = self.session.query(AnnotationSession)\
+            .filter(AnnotationSession.annotation_type==AnnotationType.POLYGON_SEQUENCE)\
+            .filter(AnnotationSession.active==1).distinct(PolygonSequence.FK_session_id).all()
+        information = [[i.FK_prep_id,i.user,i.brain_region.abbreviation] for i in active_sessions]
         return np.unique(information)
     
     def get_volume(self,prep_id,annotator_id,structure_id):
