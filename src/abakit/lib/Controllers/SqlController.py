a057156694b839ba8e016db491896a6f054f7809
madwilliam5225 <madwilliam5225@gmail.com>
2022-06-06 11:15:24 -0700

    fixes

--- src/abakit/lib/Controllers/SqlController.py
+++ src/abakit/lib/Controllers/SqlController.py
@@ -15,13 +15,10 @@ from abakit.lib.Controllers.ScanRunController import ScanRunController
 from abakit.lib.Controllers.SectionsController import SectionsController
 from abakit.lib.Controllers.SlideController import SlideController
 from abakit.lib.Controllers.SlideCZIToTifController import SlideCZIToTifController
-from abakit.lib.Controllers.TasksController import TasksController,file_processed,set_file_completed
+from abakit.lib.Controllers.TasksController import TasksController
 from abakit.model.scan_run import ScanRun
 from abakit.model.histology import Histology
 from collections import OrderedDict
-from datetime import datetime
-import numpy as np
-from sqlalchemy import func
 from sqlalchemy.orm.exc import NoResultFound
 from abakit.settings import host,schema
 
