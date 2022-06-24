2e4b5ed9cb4f30b0695c36a913c56f23feae3867
madwilliam5225 <madwilliam5225@gmail.com>
2022-06-24 08:03:14 -0700

    controller updates

--- src/abakit/lib/Controllers/Controller.py
+++ src/abakit/lib/Controllers/Controller.py
@@ -2,16 +2,33 @@ from abakit.settings import user,password,host,schema
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
-
-def create_session(host,schema):
-    connection_string = f'mysql+pymysql://{user}:{password}@{host}/{schema}?charset=utf8'
-    engine = create_engine(connection_string, echo=False)
-    Session = sessionmaker(bind=engine)
+from time import sleep
+from sqlalchemy.exc import OperationalError
+def create_session(host,schema,max_attempt = 50):
+    success = False
+    attempt=0
+    while not success and attempt<max_attempt:
+            try:
+                connection_string = f'mysql+pymysql://{user}:{password}@{host}/{schema}?charset=utf8'
+                engine = create_engine(connection_string, echo=False)
+                Session = sessionmaker(bind=engine)
+                success  = True
+            except OperationalError:
+                attempt+=1
+                sleep(5)
     return Session()
 
-def create_pooled_session(host,schema):
-    connection_string = f'mysql+pymysql://{user}:{password}@{host}/{schema}?charset=utf8'
-    pooledengine = create_engine(connection_string, pool_size=10, max_overflow=50, pool_recycle=3600)
+def create_pooled_session(host,schema,max_attempt = 50):
+    success = False
+    attempt=0
+    while not success and attempt<max_attempt:
+            try:
+                connection_string = f'mysql+pymysql://{user}:{password}@{host}/{schema}?charset=utf8'
+                pooledengine = create_engine(connection_string, pool_size=10, max_overflow=50, pool_recycle=3600)
+                success  = True
+            except OperationalError:
+                attempt+=1
+                sleep(5)
     return scoped_session(sessionmaker(bind=pooledengine)) 
 
 class Controller(object):
@@ -25,6 +42,7 @@ class Controller(object):
     def get_session(self):
         session = create_session(self.host,self.schema)
         pooledsession = create_pooled_session(self.host,self.schema)
+        success = True
         return session,pooledsession
 
     def update_row(self, row):
