ce31ed6c02afba067362d47b561a884e2d83b0e5
madwilliam5225 <madwilliam5225@gmail.com>
2022-06-26 13:54:31 -0700

    up

--- src/abakit/lib/UrlGenerator.py
+++ src/abakit/lib/UrlGenerator.py
@@ -8,34 +8,30 @@ class UrlGenerator:
         self.controller = SqlController('DK52')
     
     def add_stack_image(self,animal,channel,name=None,color = None):
+        if not hasattr(self,'dimensions'):
+            self.dimensions = self.controller.get_resolution(animal)/(10**6)
         if name == None:
-            name = f'{animal}_CH{channel}'
-        if not hasattr(self,'dims'):
-            self.dims = self.controller.get_resolution('DK52')*10^6
+            name = animal
         rgb_code = dict(red = 'vec3(pix,0,0)',green = 'vec3(0,pix,0)')
         source = f'precomputed://https://activebrainatlas.ucsd.edu/data/{animal}/neuroglancer_data/C{channel}'
         if color is not None:
             shader = '''#uicontrol invlerp normalized  (range=[0,5000])
                         #uicontrol float gamma slider(min=0.05, max=2.5, default=1.0, step=0.05)
                         #uicontrol bool colour checkbox(default=true)
-
                         void main() {
                             float pix =  normalized();
                             pix = pow(pix,gamma);
-
                             if (colour) {
                             emitRGB(vec3('''+rgb_code[color]+'''));
                             } else {
                             emitGrayscale(pix) ;
                             }
-
                         }
                         '''
         else:
             shader ='''
                     #uicontrol invlerp normalized
                     #uicontrol float gamma slider(min=0.05, max=2.5, default=1.0, step=0.05)
-
                     void main() {
                         float pix =  normalized();
                         pix = pow(pix,gamma);
@@ -61,14 +57,14 @@ class UrlGenerator:
         self.layers.append(image_layer)
     
     def add_annotation_layer(self,name,color_hex= None,annotations = None):
-        if annotations:
-            annotation_layer = dict(type =  "annotation",
-                                    source=  dict(  url = "local://annotations",
-                                                    transform = {}),
-                                    name = name)
-        if annotations:
+        annotation_layer = dict(type =  "annotation",
+                                source=  dict(  url = "local://annotations"),
+                                tab = 'annotations',
+                                annotations = [],
+                                name = name)
+        if annotations is not None:
             annotation_layer['annotations'] = annotations
-        if color_hex != None:
+        if color_hex is not None:
             annotation_layer = self.insert_annotation_color_hex(annotation_layer,color_hex)
         self.layers.append(annotation_layer)
 
@@ -79,14 +75,15 @@ class UrlGenerator:
         return annotation_layer
 
     def get_url(self):
-        if hasattr(self,'dims'):
-            return json.dumps({"dimensions": {"x": [self.dims[0],"m"],"y": [self.dims[1],"m"],"z": [self.dims[2],"m"]},'layers': self.layers})
+        if hasattr(self,'dimensions'):
+            return json.dumps({'dimensions':{'x':[self.dimensions[0],'m'],'y':[self.dimensions[1],'m'],'z':[self.dimensions[2],'m']},'layers': self.layers})
         else:
             return json.dumps({'layers': self.layers})
     
     def add_to_database(self,title,person_id):
         content = self.get_url()
-        return self.controller.add_url(content,title,person_id)
+        id = self.controller.add_url(content,title,person_id)
+        return id
     
     def parse_url(self,url):
         url = json.loads(url.replace('\n',''))
@@ -115,5 +112,3 @@ class UrlGenerator:
         for annotation in annotations:
             points.append(annotation['point'])
         return np.array(points)
-
-    
